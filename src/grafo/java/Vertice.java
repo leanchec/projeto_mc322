97f21769c049894c755238c38cff8161ba0c2a0c
leanchec <leandro.checchio@gmail.com>
2025-06-17 16:26:57 -0300

    adding first class

---
+++ src/grafo/java/Vertice.java
@@ -0,0 +1,39 @@
+package src.grafo.java;
+
+import java.util.ArrayList;
+
+public abstract class Vertice {
+    private String nome;
+    private String descricao;
+    private final ArrayList<Vertice> Vizinhos;
+
+    public Vertice(String l_nome, String l_descricao) {
+        this.nome = l_nome;
+        this.descricao = l_descricao;
+        Vizinhos = new ArrayList<>();
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public String getDescricao() {
+        return this.descricao;
+    }
+
+    public ArrayList<Vertice> getVizinhos() {
+        return this.Vizinhos;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public void AdicionarVizinho(Vertice viz) {
+        this.Vizinhos.add(viz);
+    }
+}
