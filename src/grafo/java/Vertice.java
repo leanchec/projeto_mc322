d01eacafab8cb950fb094f535c28528b35d1caab
leanchec <leandro.checchio@gmail.com>
2025-06-22 19:55:42 -0300

    updating Vertice.java

--- src/grafo/java/Vertice.java
+++ src/grafo/java/Vertice.java
@@ -5,11 +5,13 @@ import java.util.ArrayList;
 public abstract class Vertice {
     private String nome;
     private String descricao;
+    public final TipoVertice tipo;
     private final ArrayList<Vertice> Vizinhos;
 
-    public Vertice(String l_nome, String l_descricao) {
+    public Vertice(String l_nome, String l_descricao, TipoVertice l_tipo) {
         this.nome = l_nome;
         this.descricao = l_descricao;
+        this.tipo = l_tipo;
         Vizinhos = new ArrayList<>();
     }
 
