62fbe61dda3863d760c7eab7f09f685f70a54829
Samuel Rodrigues Ferreira <195440@dac.unicamp.br>
2025-06-29 18:02:58 -0300

    Adding EntidadeBase abstract class

---
+++ src/main/java/com/campanha/rpg/types/EntidadeBase.java
@@ -0,0 +1,26 @@
+package com.campanha.rpg.types;
+
+import java.util.HashMap;
+
+import com.campanha.rpg.model.Template;
+
+public abstract class EntidadeBase {
+
+    public abstract String getNome();
+    public abstract String getDescricao();
+    public abstract HashMap<String, String> getCaracteristica_String();
+    public abstract HashMap<String, Pair> getCaracteristica_Inteiros();
+
+    public abstract void setNome(String nome);
+    public abstract void setDescricao(String descricao);
+    public abstract void AdicionarCaracteristica_String(String chave, String valor);
+    public abstract void AdicionarCaracteristica_Inteiros(String chave, int valor, int limite);
+    public abstract void EditarCaracteristica_String(String chave, String valor);
+    public abstract void EditarCaracteristica_Inteiros(String chave, int valor, int limite);
+    public abstract void RemoverCaracteristica_String(String chave);
+    public abstract void RemoverCaracteristica_Inteiros(String chave);
+    public abstract void Transferir(Entidade destino, Template t);
+    public abstract void Transferir(Entidade destino);
+
+    public abstract TipoEntidade getTipoEntidade();
+}
