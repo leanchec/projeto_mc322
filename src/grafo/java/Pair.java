50f95100b109766befe19ab84b130e5e900d33fa
leanchec <leandro.checchio@gmail.com>
2025-06-29 07:06:18 -0300

    creating Pair.java

---
+++ src/grafo/java/Pair.java
@@ -0,0 +1,27 @@
+package src.grafo.java;
+
+public class Pair {
+    private int first;
+    private int second;
+
+    public Pair(int l_first, int l_second) {
+        this.first = l_first;
+        this.second = l_second;
+    }
+
+    public int getFirst() {
+        return this.first;
+    }
+
+    public int getSecond() {
+        return this.second;
+    }
+
+    public void setFirst(int first) {
+        this.first = first;
+    }
+
+    public void setSecond(int second) {
+        this.second = second;
+    }
+}
